9738eabdb7b00fd3cf309fef77c791a34d9ba834
Zenun-Vee <zenunvucetovic@gmail.com>
2017-10-28 11:40:42 -0400

    COMPLETED

--- RescueSec/RescueSec/Controller/MainViewController.swift
+++ RescueSec/RescueSec/Controller/MainViewController.swift
@@ -25,7 +25,8 @@ class MainViewController: UIViewController, CLLocationManagerDelegate {
     let locationManager = CLLocationManager()
 
     
-
+    @IBOutlet weak var markedSafeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -40,52 +41,55 @@ class MainViewController: UIViewController, CLLocationManagerDelegate {
             locationManager.startUpdatingLocation()
         }
 
+        markedSafeButton.layer.cornerRadius = 5
+        
         ref = Database.database().reference()
 
         
         var ref2 = Database.database().reference().child("/safety checks/regions/\(currentRegion)")
         
         
-        var refHandle = ref2.observe(DataEventType.value, with: { (snapshot) in
-            print("ADDED SAFETY CHECK")
-            
-            
-                if self.firstView != 1 {
-                var values = snapshot.value as? [String: [String:String]]
-                var hello = values!.sorted{ $0.key > $1.key }
-            
-                var size = hello.count - 1
-                var cnt = 0
-                
-                    let alertController = UIAlertController(title: hello.first!.value["title"]!, message: hello.first!.value["message"]!, preferredStyle: .alert)
-                            let safeAction = UIAlertAction(title: "SAFE", style: .default, handler: { (action) in
-                                //send back i am safe
-                                ref2.child("\(hello.first!.key)/isSafe").setValue("true")
-                                self.dismiss(animated: true, completion: nil)
-                            })
-                            let notSafe = UIAlertAction(title: "NOT SAFE", style: .default, handler: { (action) in
-                                ref2.child("\(hello.first!.key)/isSafe").setValue(false)
-                            })
-                            
-                            alertController.addAction(safeAction)
-                            alertController.addAction(notSafe)
-                            
-                            
-                            self.present(alertController, animated: true, completion: nil)
-                        
-                    
-                    
-                
-                
-            }
-            // ...
-            
-            
-            if self.firstView == 1 {
-                self.firstView = self.firstView + 1
-            }
-        })
-        
+//        var refHandle = ref2.observe(DataEventType.value, with: { (snapshot) in
+//            print("ADDED SAFETY CHECK")
+//
+//
+//                if self.firstView != 1 {
+//                var values = snapshot.value as? [String: [String:String]]
+//                var hello = values!.sorted{ $0.key > $1.key }
+//                ref2.child("\(hello.first!.key)/isSafe").setValue("true")
+//
+//                var size = hello.count - 1
+//                var cnt = 0
+//
+//                    let alertController = UIAlertController(title: hello.first!.value["title"]!, message: hello.first!.value["message"]!, preferredStyle: .alert)
+//                            let safeAction = UIAlertAction(title: "SAFE", style: .default, handler: { (action) in
+//                                //send back i am safe
+//                                ref2.child("\(hello.first!.key)/isSafe").setValue("true")
+//                                self.dismiss(animated: true, completion: nil)
+//                            })
+//                            let notSafe = UIAlertAction(title: "NOT SAFE", style: .default, handler: { (action) in
+//                                ref2.child("\(hello.first!.key)/isSafe").setValue(false)
+//                            })
+//
+//                            alertController.addAction(safeAction)
+//                            alertController.addAction(notSafe)
+//
+//
+//                            self.present(alertController, animated: true, completion: nil)
+//
+//
+//
+//
+//
+//            }
+//            // ...
+//
+//
+//            if self.firstView == 1 {
+//                self.firstView = self.firstView + 1
+//            }
+//        })
+//
         // Do any additional setup after loading the view.
     }
 
@@ -113,7 +117,57 @@ class MainViewController: UIViewController, CLLocationManagerDelegate {
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         var locValue:CLLocationCoordinate2D = manager.location!.coordinate
     }
+    
+    
+    @IBAction func markedSafeButtonTapped(_ sender: Any) {
+        ref = Database.database().reference()
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd.MM.yyyy"
+        let resultDate = formatter.string(from: date)
 
+        
+        var ref2 = Database.database().reference().child("/safety checks/regions/\(currentRegion)")
+        
+        
+        var refHandle = ref2.observe(DataEventType.value, with: { (snapshot) in
+            print("ADDED SAFETY CHECK")
+            
+            
+            
+                var values = snapshot.value as? [String: [String:String]]
+                var hello = values!.sorted{ $0.key > $1.key }
+                ref2.child("\(hello.first!.key)/title").setValue("Safety Check")
+                ref2.child("\(hello.first!.key)/body").setValue("Zenun marked himself safe on \(resultDate)")
+                ref2.child("\(hello.first!.key)/region").setValue("Europe")
+
+
+            
+                
+                var size = hello.count - 1
+                var cnt = 0
+                
+                let alertController = UIAlertController(title: "Good job!", message: "You marked yourself safe!", preferredStyle: .alert)
+                let safeAction = UIAlertAction(title: "Thanks", style: .default, handler: { (action) in
+                    //send back i am safe
+                    self.markedSafeButton.isHidden = true
+                    self.dismiss(animated: true, completion: nil)
+                })
+            
+                alertController.addAction(safeAction)
+                
+                
+                self.present(alertController, animated: true, completion: nil)
+                
+                
+                
+                
+                
+            })
+        
+        
+    }
+    
     /*
     // MARK: - Navigation
 
