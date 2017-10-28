786bb815e602de705ef9beec169dbb59f8ba1ddb
Liang Gao <lianggao@outlook.com>
2017-10-28 09:10:30 -0400

    createOffice and addContact

--- server.js
+++ server.js
@@ -1,34 +1,26 @@
 var admin = require("firebase-admin");
+//var firebase = require("./database/index.js");
 
-// Initialize the app with a service account, granting admin privileges
 admin.initializeApp({
-  credential: admin.credential.cert("rescue-sec-e9e0b9033721.json"),
+  credential: admin.credential.cert("rescue-sec-sa.json"),
   databaseURL: "https://rescue-sec.firebaseio.com"
 });
-
-// As an admin, the app has access to read and write all data, regardless of Security Rules
 var db = admin.database();
 
-var rootRef = db.ref();
-
-// Create a new user
-function createUser(userId, firstName, lastName, email, contacts, title, locations){
-    db.ref('users/' + userId).set({
-      firstName: firstName,
-      lastName: lastName,
-      email: email,
-      contacts: {
-        cell: contacts[0],
-        home: contacts[1],
-        office: contacts[2]
-      },
-      title: title,
-      locations: {
-        current: locations[0],
-        home: locations[1],
-        previous: locations[2]
-      }
-    });
+function createOffice(region, country, fieldOffice, location) {
+  db.ref('offices/' + region + '/' + country + '/' + fieldOffice).set({
+    region: region,
+    country: country,
+    fieldOffice: fieldOffice,
+    location: location
+  });
 }
 
-createUser(123456789, "John", "Smith", "test@example.com", [1,2,3],"test", ["1,-1","2,-2","3,-3"]);
+function addContact(region, country, fieldOffice, location, type, contact) {
+  db.ref('offices/' + region + '/' + country + '/' + fieldOffice + '/contacts/' + type).push(contact);
+}
+
+// Create a new office
+createOffice('US','NY','New York','loc_holder');
+// Add contacts: emergency, medical, security
+addContact('US','NY','New York','loc_holder','emergency',8006005000);
